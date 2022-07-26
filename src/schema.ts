a80982b4081ee19442ce1cd4d43bdbb6cc1804b6
kirshach <kirshach@mail.ru>
2022-07-27 01:34:09 +0400

    1. getting started

--- src/schema.ts
+++ src/schema.ts
@@ -3,10 +3,13 @@ import path from 'path';
 
 const root = path.resolve(__dirname, '../');
 
-export const schema = makeSchema({
+const schema = makeSchema({
   types: [],
   outputs: {
     schema: path.join(root, "schema.graphql"),
     typegen: path.join(root, "nexus-typegen.ts")
   },
 });
+
+export default schema;
+
