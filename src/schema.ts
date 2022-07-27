387a4f25122aaffac8f177ba49d6d4350fca2af7
kirshach <kirshach@mail.ru>
2022-07-27 15:52:29 +0400

    2. simple query

--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,12 @@
 import { makeSchema } from 'nexus';
 import path from 'path';
 
+import { Link, LinkQuery } from './graphql';
+
 const root = path.resolve(__dirname, '../');
 
 const schema = makeSchema({
-  types: [],
+  types: [Link, LinkQuery],
   outputs: {
     schema: path.join(root, "schema.graphql"),
     typegen: path.join(root, "nexus-typegen.ts")
@@ -12,4 +14,3 @@ const schema = makeSchema({
 });
 
 export default schema;
-
